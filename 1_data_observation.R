8c3fa6be908d812ecb648e8c19f270cd9525b0e2
modhd <128158483+modhd@users.noreply.github.com>
2023-08-30 11:02:26 +0200

    Delete 1_data_observation.R

--- 1_data_observation.R
+++
@@ -1,8 +0,0 @@
-#Data observation
-#extract and count the 5 most frequent tree species
-data <- read.csv("data/EUForestspecies.csv")
-
-species <- as.data.frame(table(data$SPECIES.NAME))
-head(species[order(species$Freq, decreasing = T), ], 5)
-
-jhggj
